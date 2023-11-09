7df0d65a302b58caaf80b17a67a135d5a2e586bc
safazi <safazi@airmail.cc>
2023-11-08 22:37:51 -0600

    make log write to output

--- src/log.ts
+++ src/log.ts
@@ -4,4 +4,5 @@ const RunService = game.GetService("RunService");
 
 export const log = Log.Configure()
 	.SetMinLogLevel(RunService.IsStudio() ? LogLevel.Information : LogLevel.Warning)
+	.WriteTo(Log.RobloxOutput({ Prefix: "RK", TagFormat: "full" }))
 	.Create();
